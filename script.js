420412423065bd7c8ee7d09625c7949e8f2bc0ea
jpratham243 <142526449+jpratham243@users.noreply.github.com>
2023-10-29 23:36:28 +0530

    all file added

---
+++ script.js
@@ -0,0 +1,17 @@
+// typing animation 
+
+// var typed = new Typed(".typing",{
+//     Strings:["Web Designer","Web Developer","Grapgic Designer","YouTuber"],
+//     typeSpeed:100,
+//     Backspeed:60,
+//     loop:true
+// })
+
+
+// var typed = new typed(".typing", {
+//     strings: ["","Web Designer", "Web Developer", "Graphic Designer", "YouTuber"],
+//     typeSpeed: 100,
+//     backSpeed: 60,
+//     loop: true,
+// })
+
